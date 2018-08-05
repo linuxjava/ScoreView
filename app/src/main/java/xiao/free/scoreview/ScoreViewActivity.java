d54b6ec4e45b285df724f17646d3122a1dfd5a20
止水 <516282426@qq.com>
2018-08-05 21:44:38 +0800

    提交初始代码

---
+++ app/src/main/java/xiao/free/scoreview/ScoreViewActivity.java
@@ -0,0 +1,46 @@
+package xiao.free.scoreview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by robincxiao on 2018/7/3.
+ */
+
+public class ScoreViewActivity extends Activity {
+    private ScoreView mScoreView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_view);
+
+        mScoreView = findViewById(R.id.scoreview);
+        mScoreView.setListener(new ScoreView.IAnimListener() {
+            @Override
+            public void onScrollStart() {
+                Log.d("xiao1", "onScrollStart");
+            }
+
+            @Override
+            public void onScrollEnd() {
+                Log.d("xiao1", "onScrollEnd");
+            }
+        });
+    }
+
+    public void onStart(View view){
+        mScoreView.start();
+    }
+
+    public void onFinishWithAnim(View view){
+        mScoreView.setScore(56, true);
+    }
+
+    public void onFinishNoAnim(View view){
+        mScoreView.setScore(9, false);
+    }
+}
